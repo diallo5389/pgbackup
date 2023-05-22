bf4577a313e7109c6118449664d732861a674910
diallo5389 <realbach41@gmail.com>
2023-05-22 23:06:29 +0000

    Final

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r') as f:
 setup(
     name='pgpackup',
     version='0.1.0',
-    author='Keith Thompson',
+    author='Bachir DIALLO',
     author_email='realbach41@gmail.com',
     description='A utility for backing up PostgreSQL databases',
     long_description=long_description,
@@ -15,6 +15,7 @@ setup(
     packages=find_packages('src'),
     package_dir={'':'src'},
     install_requires=['boto3'],
+    python_requires='>=3.6', 
     entry_points={
             'console_scripts': [
                         'pgbackup=pgbackup.cli:main'
